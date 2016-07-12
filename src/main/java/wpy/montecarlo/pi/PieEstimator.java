18f01da4ffcc28badc1ae0faf947e1d47b354dc4
wpy <wpy@10.0.0.240>
2016-07-12 18:37:27 -0400

    Change formatting

--- src/main/java/wpy/montecarlo/pi/PieEstimator.java
+++ src/main/java/wpy/montecarlo/pi/PieEstimator.java
@@ -2,34 +2,34 @@ package wpy.montecarlo.pi;
 
 public class PieEstimator {
 
-	private final double SQUARE_SIZE = 100;
-	private int sampleCount = 10000;
-	
-	public PieEstimator(int count) {
-		sampleCount = count;
-	}
-	
-	public static void main(String[] args) {
-		int count = Integer.parseInt(args[0]);
-		PieEstimator est = new PieEstimator(count);
-		System.out.println(est.execute());
-	}
-	
-	public double execute() {
-		
-		int squareHitCount = 0;
-		
-		for (int i = 0; i < sampleCount; i ++) {
-			double x = Math.random() * SQUARE_SIZE;
-			double y = Math.random() * SQUARE_SIZE;
-			
-			if (Math.sqrt(x * x + y * y) < SQUARE_SIZE) {
-				squareHitCount ++;
-			}
-		}
-		
-		double pi = squareHitCount * 4.0 / sampleCount;
-		
-		return pi;
-	}
+    private final double SQUARE_SIZE = 100;
+    private int sampleCount = 10000;
+
+    public PieEstimator(int count) {
+        sampleCount = count;
+    }
+
+    public static void main(String[] args) {
+        int count = Integer.parseInt(args[0]);
+        PieEstimator est = new PieEstimator(count);
+        System.out.println(est.execute());
+    }
+
+    public double execute() {
+
+        int squareHitCount = 0;
+
+        for (int i = 0; i < sampleCount; i++) {
+            double x = Math.random() * SQUARE_SIZE;
+            double y = Math.random() * SQUARE_SIZE;
+
+            if (Math.sqrt(x * x + y * y) < SQUARE_SIZE) {
+                squareHitCount++;
+            }
+        }
+
+        double pi = squareHitCount * 4.0 / sampleCount;
+
+        return pi;
+    }
 }
